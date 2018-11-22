2528fcbe12f998127e495fff9cb898ad61939b9b
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2018-11-23 00:28:50 +0800

    add gradient direction

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,9 @@
 var myCanvas;
 var cover;
 var bigParticles1 = [];
+var bigParticles1_ = [];
 var bigParticles2 = [];
+var bigParticles2_ = [];
 var smallParticles = [];
 var s =2;
 var nums = 1000 ;
@@ -14,26 +16,33 @@ var index2;
 var px = [];
 var py = [];
 
+var px_ = [];
+var py_ = [];
+
 var px2 = [];
 var py2 = [];
 
+var px2_ = [];
+var py2_ = [];
+
 var smallcounts;
 var sum = 0;
+var sum_ = 0;
 var sum2  = 0;
-var infor;
+var sum2_ = 0;
+
 
 function p5LoadImage(dataURL){
   img = loadImage(dataURL);
   console.log(img);
   setTimeout(function(){
     resetSketch();
-  },100);
+  },500);
 }
 
 
 function preload(){
   cover = loadImage("image/hello.png");
-  infor = loadImage("image/infor.png");
 }
 
 
@@ -52,10 +61,7 @@ function setup() {
   myCanvas = createCanvas(windowWidth, windowHeight);
   background(options.Background);
 
-
-
   resetSketch();
-
 }
 
 
@@ -64,10 +70,12 @@ function resetSketch(){
   if(type == 'image'){
     img.loadPixels();
     sum2 = 0;
+    sum2_ = 0;
     imageMode(CENTER);
     for(var i = 0 ; i < nums; i++){
       smallParticles[i] = new Particle(random(width),random(height),options.SmallSize,img);
     }
+    //cover horizontal pixel get
     for(var x = 0 ; x < img.width; x+=4){
       for(var y = 0; y < img.height; y+=4){
         index2 = int((y*img.width+x))*4;
@@ -79,13 +87,27 @@ function resetSketch(){
         }
       }  
     }
+    //cover vertical pixel get
+    for(var y = 0; y < img.height; y+=4){
+      for(var x = 0 ; x < img.width; x+=4){      
+        index2 = int((y*img.width+x))*4;
+        if(brightness(img.pixels[index2]) == 100){  
+          px2_[sum2_] = x+random(-5,5);
+          py2_[sum2_] = y+random(-5,5);      
+          bigParticles2_[sum2_] = new Particle(px2_[sum2_],py2_[sum2_],options.BigSize,img);
+          sum2_ ++;
+        }
+      }  
+    }
   }else{
     cover.loadPixels();
     sum = 0;
+    sum_ = 0;
     imageMode(CENTER);
     for(var i = 0 ; i < nums; i++){
       smallParticles[i] = new Particle(random(width),random(height),options.SmallSize,cover);
     }
+    //upload image horizontal pixel get
     for(var x = 0 ; x < cover.width; x+=5){
       for(var y = 0; y < cover.height; y+=5){
         index = int((y*cover.width+x))*4;
@@ -97,55 +119,101 @@ function resetSketch(){
         }
       }  
     }
+  //upload image vertical pixel get
+  for(var y = 0; y < cover.height; y+=5){
+    for(var x = 0 ; x < cover.width; x+=5){        
+      index = int((y*cover.width+x))*4;
+      if(brightness(cover.pixels[index]) == 100){  
+        px_[sum_] = x + random(-5,5);
+        py_[sum_] = y + random(-5,5);       
+        bigParticles1_[sum_] = new Particle(px_[sum_],py_[sum_],options.BigSize,cover);
+        sum_ ++;
+      }
+    }  
   }
+}
 }
 
 
 
 function draw() {
-
   background(options.Background[0],options.Background[1],options.Background[2], 10);
 
   if(type == "image"){
     var counts = int(100- options.Nums);
+
     if(counts%2==1){
       counts=counts+1;
     }
-    for (var i = 0; i < sum2; i+=counts ) {  
-      var percent = norm(i, 0, sum2);
-      from = color(options.Color1);
-      to = color(options.Color2);
-      between = lerpColor(from, to, percent);
-      fill(between);
-      noStroke();
-      push();
-      translate(width/2-img.width/2,height/2-img.height/2); 
-      bigParticles2[i].move();
-      bigParticles2[i].checkEdges();
-      bigParticles2[i].display(options.BigSize);
-      pop();
+
+    for (var i = 0; i < sum2; i+=counts ) { 
+      if(options.Gradient == 'horizontal') {
+        var percent = norm(i, 0, sum2);  
+        from = color(options.Color1);
+        to = color(options.Color2);
+        between = lerpColor(from, to, percent);
+        fill(between);
+        noStroke();
+        push();
+        translate(width/2-img.width/2,height/2-img.height/2); 
+        bigParticles2[i].move();
+        bigParticles2[i].checkEdges();
+        bigParticles2[i].display(options.BigSize);
+        pop();
+      }else{
+        var percent = norm(i, 0, sum2);
+        from = color(options.Color1);
+        to = color(options.Color2);
+        between = lerpColor(from, to, percent);
+        fill(between);
+        noStroke();
+        push();
+        translate(width/2-img.width/2,height/2-img.height/2); 
+        bigParticles2_[i].move();
+        bigParticles2_[i].checkEdges();
+        bigParticles2_[i].display(options.BigSize);
+        pop();
+      }
     } 
-
-
-  }else{
+  }
+  else{
     var counts = int(100- options.Nums);
     if(counts%2==1){
       counts=counts+1;
     }
-    for (var i = 0; i < sum; i+=counts ) {  
-      var percent = norm(i, 0, sum);
-      from = color(options.Color1);
-      to = color(options.Color2);
-      between = lerpColor(from, to, percent);
+    
+    if(options.Gradient == 'horizontal') {
+      for (var i = 0; i < sum; i+=counts ) { 
+        var percent = norm(i, 0, sum);  
+        from = color(options.Color1);
+        to = color(options.Color2);
+        between = lerpColor(from, to, percent);
+        fill(between);
+        noStroke();
+        push();
+        translate(width/2-cover.width/2,height/2-cover.height/2); 
+        bigParticles1[i].move();
+        bigParticles1[i].checkEdges();
+        bigParticles1[i].display(options.BigSize);
+        pop();
+      }
+    }
 
-      fill(between);
-      noStroke();
-      push();
-      translate(width/2-cover.width/2,height/2-cover.height/2);
-      bigParticles1[i].move();
-      bigParticles1[i].checkEdges();
-      bigParticles1[i].display(options.BigSize);
-      pop();
+    else{
+      for (var i = 0; i < sum_; i+=counts ) { 
+        var percent = norm(i, 0, sum_);
+        from = color(options.Color1);
+        to = color(options.Color2);
+        between = lerpColor(from, to, percent);
+        fill(between);
+        noStroke();
+        push();
+        translate(width/2-cover.width/2,height/2-cover.height/2); 
+        bigParticles1_[i].move();
+        bigParticles1_[i].checkEdges();
+        bigParticles1_[i].display(options.BigSize);
+        pop();
+      }
     }
   }
 
@@ -167,54 +235,38 @@ function draw() {
     fill(between.levels[0],between.levels[1],between.levels[2]);
     smallParticles[i].display(options.SmallSize);
   }
+}
 
-    // smooth();
-    // noFill();
-    // stroke(180);
-    // ellipse(40,30,30,30);
-    // fill(180);
-    // textAlign(CENTER);
-    // textSize(18);
-    // text("?",40,37);
 
-    // if(mouseX > 20 && mouseX < 70 && mouseY > 20 && mouseY < 70){
-    //   imageMode(LEFT);
-    //   push();
-    //   scale(0.6);
-    //   image(infor,277,320);
-    //   pop();
-    // }
+function mask(img){
+  fill(options.Background);
+  noStroke();
+  var h = (height-img.height)/2;
+  var w = (width -img.width)/2;
+  rect(0,0,width,h);
+  rect(0,0,w,height);
+  rect(width-w,0,w,height);
+  rect(0,height-h,width,h);
+}
+
+
+
+function Particle(x, y, r,img) {
+  this.loc = new p5.Vector(x, y);
+  this.vel = new p5.Vector(0, 0);
+  this.dir = new p5.Vector(0, 0);
+  this.speed = random(0.5,1);
+  this.run = function(r){
+    this.move();
+    this.checkEdges();
+    this.update(r);
   }
 
-  function mask(img){
-    fill(options.Background);
-    noStroke();
-    var h = (height-img.height)/2;
-    var w = (width -img.width)/2;
-    rect(0,0,width,h);
-    rect(0,0,w,height);
-    rect(width-w,0,w,height);
-    rect(0,height-h,width,h);
-  }
-
-
-
-  function Particle(x, y, r,img) {
-    this.loc = new p5.Vector(x, y);
-    this.vel = new p5.Vector(0, 0);
-    this.dir = new p5.Vector(0, 0);
-    this.speed = random(0.5,1);
-    this.run = function(r){
-      this.move();
-      this.checkEdges();
-      this.update(r);
-    }
-
-    this.move = function() {
+  this.move = function() {
       //noise 影响angle的变化，从而影响dir和loc，noise(x,y,z);
       this.angle = noise(this.loc.x/options.noiseScale, this.loc.y/options.noiseScale, frameCount/options.noiseScale)*TWO_PI;
       this.dir.x = cos(this.angle);//dir.x的变化
-      this.dir.y =sin(this.angle);//dir.y的变化
+      this.dir.y = sin(this.angle);//dir.y的变化
       this.vel = this.dir.copy();//获得前进的速度的方向？
       this.vel.mult(this.speed);//获得的速度的大小？
       this.loc.add(this.vel);//位置在向量的表示方式
@@ -231,9 +283,8 @@ function draw() {
       }
     }
   }
-  // this.mask = function(){   
 
-  // }
+
   this.checkEdges2 = function(){
     var h = (height-img.height)/2;
     var w = (width -img.width)/2;    
@@ -242,17 +293,18 @@ function draw() {
       this.loc.x = random(width);
       this.loc.y = random(height);
     }else if(this.loc.x <0  || this.loc.x > width || this.loc.y <0 || this.loc.y > height){
-       this.loc.x = random(width);
-      this.loc.y = random(height);
-    }
-  }
-
-  this.display = function(r) {
-    if(r==options.BigSize){
-      var psize = map(dist(this.loc.x, this.loc.y, x, y ),0,40,r/1.5,r); 
-    }else{
-     var psize = r;
-   }  
-   ellipse(this.loc.x, this.loc.y, psize, psize);
+     this.loc.x = random(width);
+     this.loc.y = random(height);
+   }
  }
+
+
+ this.display = function(r) {
+  if(r==options.BigSize){
+    var psize = map(dist(this.loc.x, this.loc.y, x, y ),0,40,r/1.5,r); 
+  }else{
+   var psize = r;
+ }  
+ ellipse(this.loc.x, this.loc.y, psize, psize);
+}
 }
